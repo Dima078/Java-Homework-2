02d4afea10610db0c35602bb2afb490050f479a0
Dmitry Kovtun <mr_write@mail.ru>
2021-12-26 19:04:39 +0300

    Homework2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 1200;
+        int Mile = TicketPrice/20;
+        System.out.println("Вам начислено " + Mile+ " бонусов");
+    }
+}
