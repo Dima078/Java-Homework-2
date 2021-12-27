a8ad9e589f3a753dbd5df7b1058ca9e12be009c1
Dmitry Kovtun <mr_write@mail.ru>
2021-12-27 10:15:00 +0300

    task1-2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 1200;
-        int Mile = TicketPrice/20;
-        System.out.println("Вам начислено " + Mile+ " бонусов");
+        int ticketPrice = 1200;
+        int mile = ticketPrice/20;
+        System.out.println("Вам начислено " + mile+ " бонусов");
     }
 }
